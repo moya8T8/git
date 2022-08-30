0cdcd16bd83e1c893f2d13bdfb90cc33300ea8bb
moya <moyamoyaa88@gmail.com>
2022-08-30 23:51:40 +0900

    8월30일

---
+++ doIt/chapter5/9.equailty.js
@@ -0,0 +1,21 @@
+// 동등비교 연산자 
+// == 값이 같음 
+// != 값과 값이 다름 
+
+console.log( 2 ==2); //t
+console.log(2!=2); //f
+console.log(2!=3); //t
+console.log(2==3);//f
+console.log(2 == '2')//t
+console.log(2 ==='2')//f 
+console.log(true == 1) //t 
+
+const obj1 = { 
+    name : 'js',
+};
+
+const obj2 = {
+    name : 'js',
+}
+
+//console.log(ojb1)
