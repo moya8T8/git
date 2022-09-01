0e9d6155fff99b1748832a6fe76883a47c57c9a4
moya <moyamoyaa88@gmail.com>
2022-09-01 23:45:28 +0900

    9월1일

---
+++ doIt/chapter6/1.if.js
@@ -0,0 +1,20 @@
+// 조건문 Conditional Statement
+//if(조건) {}
+//if(조건) {}
+
+let fruit = 'orange';
+if(fruit ==='apple'){
+    console.log('🎈')
+}else if(fruit ==='orange'){
+    console.log('어륀지');
+}else{
+    console.log('띠용')
+}
+
+if(false){
+    console.log('출력되면 안됨!')
+}else if(0){
+    console.log('이것도 출력안됨')
+}else if( 2 < 1){
+    console.log('이것도 안 됨 조건이 False 잖아!')
+}
