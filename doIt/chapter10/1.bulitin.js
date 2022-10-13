5426e7031e9c47057bcac7aefb91d9caaa75fc83
moya <moyamoyaa88@gmail.com>
2022-10-14 07:11:53 +0900

    10월13일

--- doIt/chapter10/1.bulitin.js
+++ doIt/chapter10/1.bulitin.js
@@ -12,3 +12,17 @@
 
 사용자 정의 객체 User-Defined Objects 
 */
+
+//래퍼 객체
+//원시값을 필요에 따라서 관련된 빌트인 객체로 변환한다. 
+const number = 123;
+//number 원시타입을 감싸고 있는 Number 객체로 감쌈
+console.log(number.toString());
+console.log(number); //number 원시 타입이 출력됨 
+
+const text = 'text';
+console.log(text);
+text.length;
+text.trim();l
+
+
