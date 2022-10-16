4110bc935fd4f1619ba8593dd64efe720a3640e1
moya <moyamoyaa88@gmail.com>
2022-10-16 11:57:48 +0900

    10월16일

---
+++ doIt/chapter10/3.boolean.js
@@ -0,0 +1,23 @@
+//Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf);
+
+/**
+ * Falshy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * [] //-> 뭔가 되게 false같이 생겼다 ㅋㅋㅋ 
+ * {} 
+ */
